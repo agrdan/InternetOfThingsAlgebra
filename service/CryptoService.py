2beecd3d956c8af9b67b993f7947fbbd949b4af7
DESKTOP-NCSVQ4R\Bradarius <andreas.grdjan@gmail.com>
2022-06-13 16:23:43 +0200

    encryption update

--- service/CryptoService.py
+++ service/CryptoService.py
@@ -8,46 +8,41 @@ from Crypto.Random import get_random_bytes
 class CryptoService:
 
     def __init__(self):
-        #self.pwd = "Međ1kN3t!123"
-        self.pwd = "Algebra1!123"
-        self.config = None
+        pass
 
+    def encrypt(self, plain_text, password):
+        # generate a random salt
+        salt = get_random_bytes(AES.block_size)
 
-    def encrypt(self, plain_text):
-
-        salt = "Algebra1PythonDev!".encode('utf-8')
         # use the Scrypt KDF to get a private key from the password
-        private_key = hashlib.scrypt(self.pwd.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+        private_key = hashlib.scrypt(
+            password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
 
-        test_nonce = b'\x0eL\xa9.\\\xaa\x95\x9eB\x163f\x02g\x8c\x94' # ->
-        cipher_config = AES.new(private_key, AES.MODE_SIV)
-        cipher_config.nonce = test_nonce
+        # create cipher config
+        cipher_config = AES.new(private_key, AES.MODE_GCM)
+
         # return a dictionary with the encrypted text
         cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
-
-        self.config = {
+        return {
             'cipher_text': b64encode(cipher_text).decode('utf-8'),
             'salt': b64encode(salt).decode('utf-8'),
             'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
             'tag': b64encode(tag).decode('utf-8')
         }
 
-        return b64encode(cipher_text).decode('utf-8'), self.config
-
-
-    def decrypt(self, config):
+    def decrypt(self, enc_dict, password):
         # decode the dictionary entries from base64
-        salt = b64decode(self.config['salt'])
-        cipher_text = b64decode(self.config['cipher_text'])
-        nonce = b64decode(self.config['nonce'])
-        tag = b64decode(self.config['tag'])
+        salt = b64decode(enc_dict['salt'])
+        cipher_text = b64decode(enc_dict['cipher_text'])
+        nonce = b64decode(enc_dict['nonce'])
+        tag = b64decode(enc_dict['tag'])
 
         # generate the private key from the password and salt
         private_key = hashlib.scrypt(
-            self.pwd.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+            password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
 
         # create the cipher config
-        cipher = AES.new(private_key, AES.MODE_SIV, nonce=nonce)
+        cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
 
         # decrypt the cipher text
         decrypted = cipher.decrypt_and_verify(cipher_text, tag)
